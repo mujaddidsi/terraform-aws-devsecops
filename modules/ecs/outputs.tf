c9df5434ac095f94ee852c9d97a1beb73982b975
mujaddidsi <mujaddid.s.i@gmail.com>
2026-05-28 16:24:26 +0700

    feat(ecs): add outputs for ECS module

---
+++ modules/ecs/outputs.tf
@@ -0,0 +1,49 @@
+output "cluster_id" {
+  description = "ID of the ECS cluster"
+  value       = aws_ecs_cluster.this.id
+}
+
+output "cluster_arn" {
+  description = "ARN of the ECS cluster"
+  value       = aws_ecs_cluster.this.arn
+}
+
+output "cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.this.name
+}
+
+output "service_id" {
+  description = "ID of the ECS service"
+  value       = aws_ecs_service.this.id
+}
+
+output "service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.this.name
+}
+
+output "task_definition_arn" {
+  description = "ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.this.arn
+}
+
+output "task_execution_role_arn" {
+  description = "ARN of the ECS task execution IAM role"
+  value       = aws_iam_role.task_execution.arn
+}
+
+output "task_role_arn" {
+  description = "ARN of the ECS task IAM role"
+  value       = aws_iam_role.task.arn
+}
+
+output "security_group_id" {
+  description = "ID of the ECS tasks security group"
+  value       = aws_security_group.ecs_tasks.id
+}
+
+output "cloudwatch_log_group_name" {
+  description = "Name of the CloudWatch log group for ECS tasks"
+  value       = aws_cloudwatch_log_group.this.name
+}
